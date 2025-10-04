9269fec16cd8d5bb404b64992ae3e8bfe7d4b031
tzn <i.makarov@innopolis.university>
2025-10-05 02:11:18 +0300

    update swagger urls

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Swagger schema view - ИСПРАВЛЕНО: без renderers
+# Swagger schema view
 schema_view = get_schema_view(
    openapi.Info(
       title="Online School API",
@@ -25,11 +25,11 @@ urlpatterns = [
     # Admin panel
     path('admin/', admin.site.urls),
     
-    # API documentation - ИСПРАВЛЕНО: без renderers
+    # API documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # УДАЛИЛИ НЕПРАВИЛЬНУЮ СТРОКУ С renderers
+    path('swagger.yaml', schema_view.without_ui(cache_timeout=0, renderer_classes=[openapi.renderers.YamlRenderer]), name='schema-yaml'),
     
     # App URLs
     path('api/auth/', include('accounts.urls')),
